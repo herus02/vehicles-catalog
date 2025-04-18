20e6cd5233b5be9fc08311e1d43469635be7556f
Herus Armstrong <herus02@gmail.com>
2025-04-18 18:37:32 -0300

    Add empty list message to vehicle component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { VehiclesComponent } from "./features/vehicles/vehicles.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, VehiclesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
