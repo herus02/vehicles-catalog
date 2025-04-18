9f1d2bfdc4c3e408b6222cb1be2b7c8bef4d4890
Herus Armstrong <herus02@gmail.com>
2025-04-18 18:20:45 -0300

    Add header, title and footer to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'vehicles-catalog';
+  title = 'Catálogo de Veículos';
 }
